d20a4e482b566731aebcd4a8b0b9b07e9ee8de8e
Aayush Joshi <94084212+aayushjoshi16@users.noreply.github.com>
2022-10-18 11:03:00 -0700

    Add files via upload

--- src/LinkedListTests.java
+++ src/LinkedListTests.java
@@ -1,35 +1,53 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class LinkedListTests {
 
     @Test
-    public void testLinkedListImplementation() {
+    public void test_insert() {
+        LinkedList list = new LinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
 
-        MyLinkedList list = new MyLinkedList();
-        list = MyLinkedList.insert(list, 1);
-        list = MyLinkedList.insert(list, 2);
-        list = MyLinkedList.insert(list, 3);
-        list = MyLinkedList.insert(list, 4);
-        list = MyLinkedList.insert(list, 5);
-        list = MyLinkedList.insert(list, 6);
+        Assertions.assertNotEquals(list.getFirst().getValue(), -1);
+        Assertions.assertEquals(list.getFirst().getValue(), 1);
+        Assertions.assertEquals(list.getFirst().getNext().getValue(), 2);
+        Assertions.assertEquals(list.getFirst().getNext().getNext().getValue(), 3);
+        Assertions.assertEquals(list.getFirst().getNext().getNext().getNext().getValue(), 4);
+    }
 
-        MyLinkedList.printList(list);
+    @Test
+    public void test_check_and_delete_zeros_01() {
+        LinkedList list = new LinkedList();
+        list.insert(0);
+        list.insert(0);
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
 
-        assertEquals(list.headNode.data, 1 );
-        assertEquals(list.headNode.next.data, 2 );
-        assertEquals(list.headNode.next.next.data, 3 );
-        assertEquals(list.headNode.next.next.next.data, 4 );
+        list.check_and_delete_zeros();
+        String output = list.toString();
+        Assertions.assertEquals(output, "LinkedList: 1, 2, 3, 4");
+    }
 
-        MyLinkedList.deleteByData(list, 1);
-        MyLinkedList.printList(list);
+    @Test
+    public void test_check_and_delete_zeros_02() {
+        LinkedList list = new LinkedList();
+        list.insert(0);
+        list.insert(0);
+        list.insert(0);
+        list.insert(1);
+        list.insert(2);
+        list.insert(0);
+        list.insert(3);
 
-        assertEquals(list.headNode.data, 2 );
-        assertEquals(list.headNode.next.data, 3 );
-        assertEquals(list.headNode.next.next.data, 4 );
-        assertEquals(list.headNode.next.next.next.data, 5 );
-
+        list.check_and_delete_zeros();
+        String output = list.toString();
+        Assertions.assertEquals(output, "LinkedList: 1, 2, 0, 3");
     }
 }
